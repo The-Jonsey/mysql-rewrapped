d2f949bde8c5787843f32ac1ac2f2b13ac4b90a8
Louis Jones <louis@thejonsey.com>
2019-01-06 02:18:23 +0000

    Intial Commit

---
+++ README.md
@@ -0,0 +1,176 @@
+#MySQL-Wrapped
+
+##Description
+
+MySQL wrapped is a nodejs library built to make querying a database without any knowledge of sql easy,
+it depends on:
+- mysql
+
+##Setup
+To install this module, from the command line run
+
+`npm install --save mysql-wrapped`
+
+to include this package in your software, include this in your source code
+
+`const mysql = require("mysql-wrapped");`
+
+To initialise an instance of mysql-wrapped, you need to give it config in the form of a json object,
+as well as this, the initialisation is asynchronous, so you will need a callback e.g:
+
+```js
+const mysqlWrapped = require("mysql-wrapped");
+let mysql;
+mysqlWrapped({
+ "host": "127.0.0.1",
+ "user": "root",
+ "password": "password",
+ "database": "EXAMPLE_SCHEMA",
+ "connectionLimit": 10
+}, (data) => {
+    mysql = data;
+});
+```
+
+##Usage
+
+mysql-wrapped has 3 levels of abstraction, and it can be used at any level, with the lowest level being the default mysql package, so in ascending levels of abstraction, it could be used like this
+
+##Level 1 - No abstraction
+
+this would be used in the same way as the mysql package, and most of the best documentation would be found at that package, specifically on connection pools,
+the way the connection pool is accessed is
+
+`mysql.Database.connectionPool`
+
+##Level 2 - Query generation
+
+This level is where queries are generated, and can generate the 4 main types of query, which all have a common structure, with common functions
+
+```js
+new mysql.QUERYTYPE(
+    
+)
+```
+then you add functions on to the end depending on what you want to do with the query, but at to execute every query, you do:
+```js
+new mysql.QUERYTYPE().exec(
+    //you place a callback in here, which returns your data
+)
+```
+###SELECT
+
+To use SELECT statements with this library, you would do something along the lines of
+
+```js
+new mysql.Select(
+    //field names here, as an array, or an asterisk to indicate a wildcard
+)
+```
+
+The select statement has a special function called join, which is used as shown below
+```js
+join(
+    /*
+    Joining one table onto another, currently only INNER JOIN supported, usage as shown:
+    [{target: target table, from: field on main table, to: field on joining table}]
+     */
+)
+```
+###Update
+
+Update is very similar to the Select statement, the only difference being the constructor
+
+```js
+new mysql.Update(
+    //json object of fields to update, with their values as shown below
+    //{field: value} - e.g
+    {firstname: "Bob"}
+    
+)
+```
+
+###Insert
+For insert, the constructor is the same as for Update, so
+```js
+new mysql.Insert(
+    //json object of fields to insert, with their values as shown below
+    //{field: value} - e.g
+    {firstname: "Bob"}
+    
+)
+```
+
+###Delete
+For insert, the constructor does not take any parameters
+
+###Common Functions
+```js
+new mysql.QUERY.table(
+    //the name of the table to select from
+).where(
+    /*
+    Filtering the table, done by a json object as shown:
+    {
+        fieldname: {
+            value: value, op: operator
+        }
+    }
+    where the valid operators are "=", "<=>", "<>", "!=", ">", ">=", "<", "<=", "like"
+     */
+).exec(
+    /*
+    This function executes the generated query, and returns in via a callback function
+     */
+)
+```
+
+##Level 3 - Table generation + field checks
+
+so for level 3, the library will generate an instance of a table class for every table it finds in your database, it will use this to check your parameters, to check the fields exist in the tables.
+
+To use this functionality, all you need to do is:
+
+```
+TABLENAME.Select()
+TABLENAME.Insert()
+```
+
+the advantage of this is you also dont need to use the .table function when generating the query, as it is automatically done
+
+in actual use the code would look like this
+
+```js
+Users.select("*")
+    .where(
+        {groupname:
+            {
+                value: "Admin",
+                op: "="
+            }
+        })
+    .join(
+        [
+            {
+                target: "UserGroups",
+                from: "Users.id",
+                to: "UserGroups.userid"
+            },
+            {
+                target: "Groups",
+                from: "UserGroups.groupid",
+                to: "Groups.id"
+            }
+        ]
+    ).exec(data => {
+        console.log(data);
+    }
+);
+```
+
+
+#Planned Features
+
+- Left and right joins, as well as outer joins
+- ALTER statement
+- CREATE TABLE statement
---
+++ index.js
@@ -0,0 +1,345 @@
+const mysql = require("mysql");
+class Database {
+    constructor(conf) {
+        this.dbname = conf.database;
+        this.connectionPool = mysql.createPool(conf);
+        this.tables = {};
+        this.conf = conf;
+    }
+
+    meta(cback) {
+        console.log("hi");
+        console.log(this.conf);
+        this.connectionPool.query("SHOW TABLES;", (error, results) => {
+            if (error)
+                console.log(error);
+            if (results !== undefined) {
+                results.forEach(table => {
+                    this.tables[table["Tables_in_" + this.dbname]] = {};
+                });
+                Object.keys(this.tables).forEach((table) => {
+                    this.connectionPool.query("describe " + table, (error, results) => {
+                        results.forEach(col => {
+                            this.tables[table][col["Field"]] = col["Type"];
+                        });
+                        if (cback)
+                            cback();
+                    });
+                });
+            }
+            cback();
+        });
+    }
+}
+
+class Query {
+    /**
+     *
+     * @param type - type of query e.g "SELECT", "INSERT"
+     */
+    constructor(type) {
+        this.type = type;
+        this.whereStatement = null;
+        this.whereParams = [];
+        this.queryParams = [];
+        this.safeOperators = ["=", "<=>", "<>", "!=", ">", ">=", "<", "<=", "like"];
+    }
+
+    /**
+     *
+     * @param table - Table name
+     * @returns {Query}
+     */
+    table(table) {
+        this.tableName = table;
+        return this;
+    }
+
+    /**
+     *
+     * @param params formatted as {fieldname: {value: value, op: operator}}
+     * @returns {Query}
+     */
+    where(params) {
+        let statement = "WHERE ";
+        Object.keys(params).forEach((item) => {
+            if (this.safeOperators.includes(params[item].op.toLowerCase())) {
+                statement += item + ' ' + params[item].op + " ? AND ";
+                this.whereParams.push(params[item].value);
+            }
+            else if (params[item].op.toLowerCase() === "in") {
+                statement += item + " IN (";
+                params[item].value.forEach(val => {
+                    statement += "?, ";
+                    this.whereParams.push(val);
+                });
+                statement = statement.substr(0, statement.length - 2);
+                statement += ") AND ";
+            }
+            else {
+                throw "Invalid Comparison Operator";
+            }
+        });
+        statement = statement.substr(0, statement.length - 5);
+        this.whereStatement = statement;
+        return this;
+    }
+
+    /**
+     * Executes the query
+     * @param cback - callback function
+     * @param res - response object - optional
+     */
+    exec(cback, res) {
+        //console.log(this.toString());
+        let callback = (error, results) => {
+            if (error) {
+                console.log(error);
+                cback(false);
+            }
+            else
+                cback(results);
+        };
+        try {
+            console.log(db);
+            if (this.queryParams !== null) {
+                db.connectionPool.query(this.toString(), this.queryParams, callback)
+            }
+            else
+                db.connectionPool.query(this.toString(), callback)
+        }
+        catch(e) {
+            if (res)
+                res.send(e);
+            else
+                throw e;
+        }
+    }
+}
+
+class Select extends Query {
+    /**
+     *
+     * @param items - list if items to select formatted as either:
+     *              - ["id", "firstname", "lastname"]
+     *              - ["id", "firstname", {name: "lastname", as: "surname"}
+     */
+    constructor(items) {
+        super("SELECT");
+        if (!items) {
+            items = "*";
+        }
+        else {
+            if (typeof items === "string")
+                items = JSON.parse(items);
+            for (let i = 0; i < items.length; i++) {
+                if (typeof items[i] === "object") {
+                    items[i] = items[i].name + " AS " + items[i].as;
+                }
+            }
+            items = items.join(", ");
+        }
+        this.items = items;
+        this.joinStatement = null;
+    }
+
+    /**
+     *
+     * @param params join formatted as [{target: target table, from: field on main table, to: field on joining table}]
+     * @returns {Select}
+     */
+    join(params) {
+        if (params.length > 2) {
+            return false;
+        }
+        let statement = "";
+        params.forEach(item => {
+            statement += "INNER JOIN " + item.target + " ON " + item.from + " = " + item.to + " " ;
+        });
+        this.joinStatement = statement;
+        return this;
+    }
+
+    toString() {
+        return this.type + " " + this.items + " FROM " + this.tableName + " " + (this.joinStatement !== null ? this.joinStatement + " " : "") + (this.whereStatement !== null ? this.whereStatement : "")
+    }
+
+    exec(cback) {
+        this.queryParams = this.whereParams;
+        super.exec(cback);
+    }
+}
+
+class Update extends Query {
+
+    /**
+     *
+     * @param fields - list of fields to update formatted as {field: value}
+     */
+    constructor(fields) {
+        super("UPDATE");
+        this.setStatement = "";
+        this.setParams = [];
+        Object.keys(fields).forEach(item => {
+            this.setStatement += item + " = ?, ";
+            this.setParams.push(fields[item]);
+        });
+    }
+
+    toString() {
+        return this.type + " " + this.tableName + " " + this.setStatement + (this.whereStatement !== null ? this.whereStatement : "");
+    }
+
+    exec(cback) {
+        this.queryParams = this.setParams.concat(this.whereParams);
+        super.exec(cback);
+    }
+}
+
+class Insert extends Query {
+    /**
+     *
+     * @param fields - list of fields to insert formatted as {field: value}
+     */
+    constructor(fields) {
+        super("INSERT");
+        this.fields = Object.keys(fields).join(", ");
+        this.valuesParams = [];
+        this.values(fields);
+    }
+
+    /**
+     *
+     * @param fields - fields to insert formatted as {field: value}
+     * @returns {Insert}
+     */
+    values(fields) {
+        let rowParsed = Array(Object.keys(fields).length + 1).join("?, ");
+        rowParsed = rowParsed.substr(0, rowParsed.length - 2);
+        Object.keys(fields).forEach((item) => {
+            this.valuesParams.push(fields[item]);
+        });
+        this.valuesStatement = rowParsed;
+        return this;
+    }
+
+    toString() {
+        return "INSERT INTO " + this.tableName + " (" + this.fields + ") VALUES (" + this.valuesStatement + ")"
+    }
+
+    exec(cback) {
+        this.queryParams = this.valuesParams;
+        super.exec(cback)
+    }
+}
+
+class Delete extends Query {
+    constructor() {
+        super("DELETE");
+    }
+
+    toString() {
+        return "DELETE FROM " + this.tableName + (this.whereStatement !== null ? this.whereStatement : "");
+    }
+
+    exec(cback) {
+        this.queryParams = this.whereParams;
+        super.exec(cback);
+    }
+}
+
+class InvalidFieldError extends Error {
+    constructor(args) {
+        super(args);
+        Error.captureStackTrace(this, InvalidFieldError);
+    }
+}
+
+class Table {
+    constructor(name, fields) {
+        this.name = name;
+        this.fields = fields;
+    }
+
+    Select(fields) {
+        let passed = true;
+        if (fields && fields !== ["*"] && fields !== "*") {
+            fields.forEach(field => {
+                if (field instanceof Object) {
+                    field = field.name;
+                }
+                if (!this.fields.includes(field) && !field.includes("."))
+                    passed = false;
+                else if (field.includes(".")) {
+                    let table = field.split(".")[0];
+                    let attr = field.split(".")[1];
+                    if (!module.exports[table] || !module.exports[table].fields.includes(attr)) {
+                        passed = false;
+                    }
+                }
+            });
+        }
+        else {
+            fields = ["*"];
+        }
+        if (!passed) {
+            throw new InvalidFieldError("Field not found in table");
+        }
+        else {
+            return new Select(fields).table(this.name);
+        }
+    }
+
+    Update(items) {
+        let passed = true;
+        Object.keys(items).forEach((field) => {
+            if (!this.fields.includes(field))
+                passed = false;
+        });
+        if (!passed) {
+            throw new InvalidFieldError("Field not found in table");
+        }
+        else {
+            return new Update(items).table(this.name);
+        }
+    }
+
+    Insert(items) {
+        let passed = true;
+        Object.keys(items).forEach((field) => {
+            if (!this.fields.includes(field))
+                passed = false;
+        });
+        if (!passed) {
+            throw new InvalidFieldError("Field not found in table");
+        }
+        else {
+            return new Insert(items).table(this.name);
+        }
+    }
+
+    Delete() {
+        return new Delete().table(this.name);
+    }
+
+}
+let db;
+module.exports = function(config, cback) {
+    db = new Database(config);
+    let returning = {
+        Select,
+        Insert,
+        Update,
+        Delete,
+        InvalidFieldError,
+        Database: db,
+    };
+    db.meta(() => {
+        setTimeout(() => {
+            Object.keys(db.tables).forEach(table => {
+                returning[table] = new Table(table, Object.keys(db.tables[table]));
+            });
+            cback(returning);
+        }, 1000);
+    });
+};
